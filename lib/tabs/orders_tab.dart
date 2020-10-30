796f408d5ee1a8f2ed58165c210fdbbc41d1db27
Felipe Sales <[soufeliposales@gmail.com]>
2020-10-30 11:32:53 -0300

    displaying a list of orders

--- lib/tabs/orders_tab.dart
+++ lib/tabs/orders_tab.dart
@@ -1,16 +1,35 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import '../blocs/orders_bloc.dart';
 import '../tiles/order_tile.dart';
 
 class OrdersTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _ordersBloc = BlocProvider.of<OrdersBloc>(context);
+
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 16),
-      child: ListView.builder(
-        itemCount: 6,
-        itemBuilder: (context, index) {
-          return OrderTile();
-        },
+      child: StreamBuilder<List>(
+        stream: _ordersBloc.outOrders,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation(
+                  Theme.of(context).accentColor)));
+          } else if (snapshot.data.length == 0) {
+            return Center(
+              child: Text('Nenhum pedido encontrado!', 
+                style: TextStyle(color: Theme.of(context).accentColor)));
+          }
+          return ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (context, index) {
+              return OrderTile(snapshot.data[index]);
+            },
+          );
+        }
       ),
     );
   }
--- lib/tiles/order_tile.dart
+++ lib/tiles/order_tile.dart
@@ -1,7 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../widgets/order_header.dart';
 
 class OrderTile extends StatelessWidget {
+  final DocumentSnapshot order;
+  OrderTile(data, {this.order});
+
+  final states = [
+    '',
+    'Em preparação',
+    'Em transporte',
+    'Aguardando entrega',
+    'Entregue'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -9,50 +21,57 @@ class OrderTile extends StatelessWidget {
       child: Card(
         child: ExpansionTile(
           title: Text(
-            '#vs gt - Entregue',
-            style: TextStyle(color: Colors.green),
+            // ignore: lines_longer_than_80_chars
+            '#${order.documentID.substring(order.documentID.length - 7, order.documentID.length)} - '
+            '${states[order.data['status']]}',
+            style: TextStyle(color: order.data['status'] != 4 
+              ? Colors.grey[850]
+              : Colors.green
+            ),
           ),
           children: <Widget>[
             Padding(
-            padding: EdgeInsets.fromLTRB(16, 0, 16, 8),
-              child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  OrderHeader(),
-                  Column(
-                    mainAxisSize: MainAxisSize.min, 
-                    children: <Widget>[
-                      ListTile(
-                      title: Text('Camiseta'),
-                      subtitle: Text('Camisetas/sgvejhenbrt'),
-                      trailing: Text('2', style: TextStyle(fontSize: 20)),
-                      contentPadding: EdgeInsets.zero,
-                      ),
-                    ]
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      FlatButton(
-                        onPressed: () {},
-                        child: Text('Excluir',
-                          style: TextStyle(color: Colors.red)),
-                      ),
-                      FlatButton(
-                        onPressed: () {},
-                        child: Text('Regredir',
-                          style: TextStyle(color: Colors.grey)),
-                      ),
-                      FlatButton(
-                        onPressed: () {},
-                        child: Text('Avançar',
-                          style: TextStyle(color: Colors.green)),
-                       ),
-                    ],    
-                  ),
-                ],
-              )
-            ),
+                padding: EdgeInsets.fromLTRB(16, 0, 16, 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    OrderHeader(),
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: order.data['products'].map<Widget>((p) {
+                        return ListTile(
+                          // ignore: prefer_interpolation_to_compose_strings
+                          title: Text(p['product']['title'] + ' ' + p['size']),
+                          // ignore: prefer_interpolation_to_compose_strings
+                          subtitle: Text(p['category'] + '/' + p['productId']),
+                          trailing: Text(p['quantity'], 
+                            style: TextStyle(fontSize: 20)),
+                          contentPadding: EdgeInsets.zero,
+                        );
+                      }).toList(),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        FlatButton(
+                          onPressed: () {},
+                          child: Text('Excluir',
+                              style: TextStyle(color: Colors.red)),
+                        ),
+                        FlatButton(
+                          onPressed: () {},
+                          child: Text('Regredir',
+                              style: TextStyle(color: Colors.grey)),
+                        ),
+                        FlatButton(
+                          onPressed: () {},
+                          child: Text('Avançar',
+                              style: TextStyle(color: Colors.green)),
+                        ),
+                      ],
+                    ),
+                  ],
+                )),
           ],
         ),
       ),
