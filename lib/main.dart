bd953e4941d1dd8dbef13c7ab6eb541369928c51
Felipe Sales <[soufeliposales@gmail.com]>
2020-10-31 14:54:33 -0300

    updates in codes and formating

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
-//import 'screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
--- lib/widgets/input_field.dart
+++ lib/widgets/input_field.dart
@@ -13,23 +13,24 @@ class InputField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<String>(
-        stream: stream,
-        builder: (context, snapshot) {
-          return TextField(
-            onChanged: onChanged,
-            decoration: InputDecoration(
-              icon: Icon(icon, color: Colors.white70),
-              hintText: hint,
-              hintStyle: TextStyle(color: Colors.white),
-              focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: Theme.of(context).accentColor),
-              ),
-              contentPadding: EdgeInsets.fromLTRB(5, 30, 30, 30),
-              errorText: snapshot.hasError ? snapshot.error : null,
+      stream: stream,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: onChanged,
+          decoration: InputDecoration(
+            icon: Icon(icon, color: Colors.white70),
+            hintText: hint,
+            hintStyle: TextStyle(color: Colors.white),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Theme.of(context).accentColor),
             ),
-            style: TextStyle(color: Colors.white),
-            obscureText: obscure,
-          );
-        });
+            contentPadding: EdgeInsets.fromLTRB(5, 30, 30, 30),
+            errorText: snapshot.hasError ? snapshot.error : null,
+          ),
+          style: TextStyle(color: Colors.white),
+          obscureText: obscure,
+        );
+      }
+    );
   }
 }
