9832c475b1026fb8f81875adc7145e023edada1b
Felipe Sales <[soufeliposales@gmail.com]>
2020-11-17 11:19:10 -0300

    displaying product title, description and price

--- lib/blocs/product_bloc.dart
+++ lib/blocs/product_bloc.dart
@@ -1,11 +1,32 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rxdart/rxdart.dart';
 
 class ProductBloc extends BlocBase {
+  final _dataController = BehaviorSubject<Map>();
+  Stream<Map> get outData => _dataController.stream;
   String categoryId;
   DocumentSnapshot product;
-  ProductBloc({this.categoryId, this.product});
+  Map<String, dynamic> unsavedData;
+  ProductBloc({this.categoryId, this.product}) {
+    if (product != null) {
+      unsavedData = Map.of(product.data);
+      unsavedData['images'] = List.of(product.data['images']);
+      unsavedData['sizes'] = List.of(product.data['sizes']);
+    } else {
+      unsavedData = {
+        'title': null,
+        'description': null,
+        'price': null,
+        'images': [],
+        'sizes': [], 
+      };
+    }
+    _dataController.add(unsavedData);
+  }
 
   @override
-  void dispose() {}
+  void dispose() {
+    _dataController.close();
+  }
 }
